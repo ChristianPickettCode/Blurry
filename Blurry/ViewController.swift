07eb8ad716f7d0eef838b6d0b1617e29f5c2d201
Christian Pickett <christian.pickett@mail.mcgill.ca>
2018-02-03 16:02:42 -0500

    Initial Commit

---
+++ Blurry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blurry
+//
+//  Created by Chris Pickett on 2018-02-03.
+//  Copyright © 2018 Chris Pickett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
